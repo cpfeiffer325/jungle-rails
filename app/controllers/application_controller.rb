24405107bebaa50fefda178b7d570fc14589087f
cpfeiffer325 <cpfeiffer325@gmail.com>
2019-06-14 12:47:27 -0600

    delete unused code from app controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -29,17 +29,6 @@ class ApplicationController < ActionController::Base
   end
   helper_method :cart_subtotal_cents
 
-  # def enhanced_order
-  #   @enhanced_order ||= Product.where(id: cart.keys).map {|product| { product:product, quantity: cart[product.id.to_s] } }
-  # end
-  # helper_method :enhanced_cart
-
-  # def order_subtotal_cents
-  #   enhanced_cart.map {|entry| entry[:product].price_cents * entry[:quantity]}.sum
-  # end
-  # helper_method :cart_subtotal_cents
-
-
   def update_cart(new_cart)
     cookies[:cart] = {
       value: JSON.generate(new_cart),
