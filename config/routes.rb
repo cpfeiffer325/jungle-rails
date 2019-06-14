9db4c054d337a13b3766b30b91675ae08d63e635
cpfeiffer325 <cpfeiffer325@gmail.com>
2019-06-13 18:11:41 -0600

    add routing for user and sessions

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
 
+  resources :users, only: [:new, :create]
+  resources :sessions, only: [:new, :create]
+  get '/logout' => 'sessions#destroy'
+  
   root to: 'products#index'
 
   resources :products, only: [:index, :show]
