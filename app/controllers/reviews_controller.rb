6e718647f6d01c94802fa1b38dd36f93f1fc2c4b
cpfeiffer325 <cpfeiffer325@gmail.com>
2019-06-15 16:13:16 -0600

    add index, new, create and show to reviews controller

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,2 +1,14 @@
 class ReviewsController < ApplicationController
+  def index
+    @reviews = Review.ApplicationController
+  end
+
+  def new
+  end
+
+  def create
+  end
+
+  def show
+  end
 end
