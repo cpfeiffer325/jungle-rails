707974adc2f612de70f5154015235e085d7df68e
cpfeiffer325 <cpfeiffer325@gmail.com>
2019-06-17 14:54:04 -0600

    create form for adding reviews to product

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,14 +1,39 @@
 class ReviewsController < ApplicationController
-  def index
-    @reviews = Review.ApplicationController
-  end
+  before_filter :current_user, :only => [:create, :destroy]
 
   def new
+    @review = Review.new
   end
 
   def create
+    @product= Product.find_by_id(params[:product_id])
+    # @review= @product.reviews.new(review_params)
+    @review= Review.new(review_params)
+    @review.product_id = @product.id
+    @review.rating = @review.rating.to_i
+    @review.user_id = current_user.id
+    puts "this is the review: #{@review.inspect}"
+
+    if @review.save
+      redirect_to product_path(@product)
+    else
+      redirect_to "/"
+    end
   end
 
-  def show
+  def destroy
+    # ensure user only can find owner comment.
+    @review = current_user.reviews.find(params[:id])
+    @review.destroy
+    redirect_to product_path(@product)
+  end
+
+  private
+
+  def review_params
+    params.require(:review).permit(
+      :description,
+      :rating
+    )
   end
 end
