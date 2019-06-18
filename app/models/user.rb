d7235d4ac7b2fe0d09d394d57f723b61959cd26e
cpfeiffer325 <cpfeiffer325@gmail.com>
2019-06-18 13:20:33 -0600

    add  email and password validation to model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,8 @@
 class User < ActiveRecord::Base
+  validates_presence_of :password_confirmation
+  validates_length_of :password, minimum: 6
   has_secure_password
 
-  # validates_presence_of :first_name, :last_name, :email, :password, :password_confirmation
-  # validates_uniqueness_of :email
+  validates_presence_of :first_name, :last_name
+  validates :email, presence: true, uniqueness: {case_sensitive: false}
 end
