31a65c54efd9c152ead5805748f1bca542216016
cpfeiffer325 <cpfeiffer325@gmail.com>
2019-06-18 11:54:48 -0600

    add error messages for validation

--- spec/models/product_spec.rb
+++ spec/models/product_spec.rb
@@ -5,6 +5,7 @@ RSpec.describe Product, type: :model do
 
     subject {
       @category = Category.new(name: "test")
+      @category.save
       described_class.new(
         name: "Anything",
         price_cents: 100,
@@ -19,22 +20,30 @@ RSpec.describe Product, type: :model do
 
     it "is valid with valid name" do
       subject.name = nil
+      subject.save
       expect(subject).to_not be_valid
+      expect(subject.errors.full_messages).to include("Name can't be blank")
     end
 
     it "is valid with valid price" do
       subject.price_cents = nil
+      subject.save
       expect(subject).to_not be_valid
+      expect(subject.errors.full_messages).to include("Price can't be blank")
     end
 
     it "is valid with valid quantity" do
       subject.quantity = nil
+      subject.save
       expect(subject).to_not be_valid
+      expect(subject.errors.full_messages).to include("Quantity can't be blank")
     end
 
     it "is valid with valid category" do
       subject.category = nil
+      subject.save
       expect(subject).to_not be_valid
+      expect(subject.errors.full_messages).to include("Category can't be blank")
     end
   end
 end
